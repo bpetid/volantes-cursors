5d30955ba8a0542c7d2a240ccd56426ee0f47713
Bányai Péter <bpetid@pm.me>
2025-02-26 22:17:51 +0100

    add high resolution cursor generation to build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ convert_to_png() {
 	# shellcheck disable=SC2016
 	for file in "$src_dir"/*.svg; do
 		[ -f "$file" ] || continue
-		for size in 24 32 48 64; do
+		for size in 24 30 36 42 48 54 60 66 72 78 84 90 96 102 108 114 120 126 132 138 144 150 156 162 168 174 180 186 192 198 204 210 216 222 228 234 240 246 252 258; do
 			bitmap_file="${out_dir%/}/$(basename "$file" .svg)_${size}.png"
 
 			svg_mtime="$(stat -c '%Y' "$file")"
